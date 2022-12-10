820162ceefe93e14650eaa7ad707c279ec98d81f
RainWashed <kingretracted4yt@gmail.com>
2022-12-10 10:29:31 -0500

    added another animation variant for the banner and created a localStorage toggle for it.

--- public/scripts/index.site.js
+++ public/scripts/index.site.js
@@ -53,3 +53,33 @@ function handleFavicon(x) {
 
 handleFavicon(x);
 x.addEventListener("change", handleFavicon);
+
+const animationPathParent = document.querySelector("g.strokes-parent");
+const cycleAnimBtn = document.getElementById("cycle_anim_btn");
+
+if (localStorage.getItem("cooler_anim") === null) {
+  localStorage.setItem("cooler_anim", "false");
+} else {
+  if (localStorage.getItem("cooler_anim") === "false")
+    animationPathParent.classList.remove("stroke-anim-cooler");
+  else animationPathParent.classList.add("stroke-anim-cooler");
+}
+
+cycleAnimBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  anime({
+    targets: "button#cycle_anim_btn i",
+    rotate: [0, 360],
+    duration: 1250,
+    easing: "easeOutElastic",
+  });
+
+  if (animationPathParent.classList.contains("stroke-anim-cooler")) {
+    localStorage.setItem("cooler_anim", "false");
+    animationPathParent.classList.remove("stroke-anim-cooler");
+  } else {
+    localStorage.setItem("cooler_anim", "true");
+    animationPathParent.classList.add("stroke-anim-cooler");
+  }
+});
